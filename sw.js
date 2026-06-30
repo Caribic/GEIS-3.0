0c0b27a3c210d13a89bdaf594e727f8e9ebd0264
Caribic <125411897+Caribic@users.noreply.github.com>
2026-06-30 21:27:51 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'svoz-palet-v16'; 
+const CACHE_NAME = 'svoz-palet-v17'; 
 
 // Relativní cesty zajišťují kompatibilitu na jakékoliv doméně/složce na GitHubu
 const urlsToCache = [
